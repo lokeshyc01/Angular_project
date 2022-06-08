fd9499f409a03e19390534185a09aab1521351b3
lokeshyc01 <100062082+lokeshyc01@users.noreply.github.com>
2022-06-08 13:33:01 +0530

    Add files via upload

---
+++ parenttochild/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'parent_to_child';
+
+  public Message="Hello Child Component";
+}
